a9912acad1c7d85dce27851fe64d4b0cb99fefd7
Renan Reis <renanmacfernandes@hotmail.com>
2024-03-23 21:32:14 -0300

    Client class

---
+++ src/main/java/entities/Client.java
@@ -0,0 +1,86 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String user;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+
+	public Client() {}
+	public Client(Long id, String user, String password, String name, String email, String phone) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", user=" + user + ", password=" + password + ", name=" + name + ", email=" + email
+				+ ", phone=" + phone + "]";
+	}
+		
+}
