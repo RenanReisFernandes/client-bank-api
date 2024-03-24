5dea4ed5372c5794fab6a764debccc8a267e6eac
Renan Reis <renanmacfernandes@hotmail.com>
2024-03-24 13:51:30 -0300

    updating controller

--- src/main/java/com/bank/controllers/ClientController.java
+++ src/main/java/com/bank/controllers/ClientController.java
@@ -1,20 +1,32 @@
 package com.bank.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.entities.Client;
+import com.bank.services.ClientService;
 
 @RestController
 @RequestMapping(value = "clients")
 public class ClientController {
-	
+
+	@Autowired
+	private ClientService clientService;
+
+	public ResponseEntity<Client> save(Client client) {
+		Client obj = clientService.create(client);
+		return ResponseEntity.ok().body(obj);
+	}
+
 	@GetMapping
-	public ResponseEntity<Client> findAll(){
-		Client c = new Client(1L,"ReBank","1234","renan","renan@email.com","999999");
-		return ResponseEntity.ok().body(c);
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = clientService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
 }
