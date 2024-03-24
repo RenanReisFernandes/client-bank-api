f0c9f2f04180fc4877f274f0ab5442cd61ca9eef
Renan Reis <renanmacfernandes@hotmail.com>
2024-03-24 00:56:37 -0300

    ClientController

--- src/main/java/com/bank/entities/Client.java
+++ src/main/java/com/bank/entities/Client.java
@@ -3,9 +3,17 @@ package com.bank.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String user;
 	private String password;
